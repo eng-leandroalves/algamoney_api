fea9d89735767e9f8e53b87c927d46d0f8982e67
engleandroalves <eng.leandroalves@gmail.com>
2022-01-15 10:49:31 -0300

    3.11. Criando o cadastro de pessoa

---
+++ algamoney-api/src/main/java/com/algamoney/api/resource/PessoaResource.java
@@ -0,0 +1,56 @@
+package com.algamoney.api.resource;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.algamoney.api.model.Pessoa;
+import com.algamoney.api.repository.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	
+	@GetMapping
+	public List<Pessoa> listar() {
+		return pessoaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		
+		//essa parte é necessária para passar ao Header o location do recurso recém criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(pessoaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Optional<Pessoa>> buscarPeloCodigo(@PathVariable Long codigo) {
+
+
+		Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+		return !pessoa.isEmpty() ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+	}
+
+}
